c69a02e6eb47a83a1f58bbb86a03278a77111a59
cstricklan <contact@kraetonics.com>
2011-02-13 00:15:12 -0500

    Initial Commit

---
+++ SetFigure.m
@@ -0,0 +1,7 @@
+
+function SetFigure( Figure, Name, Position )
+  set(Figure, 'color', 'w');
+  set(Figure, 'Name', Name);
+  set(Figure, 'NumberTitle', 'off');
+  set(Figure, 'Position', Position);
+end
